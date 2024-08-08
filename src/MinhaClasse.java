c63336a68a897327b61e62ea25cff01e0943ab4d
Eligiani-Monteiro <eligianedasilva32@gmail.com>
2024-08-08 09:29:05 -0400

    Anatomia das Classes

---
+++ src/MinhaClasse.java
@@ -0,0 +1,19 @@
+//Classe
+public class MinhaClasse {
+	// método principal
+	public static void main(String[] args) {
+
+		// classe de operações de input e output (Impressão)
+		// System.out.print("Olá turma , sejam bem-vindos ");
+
+		String primeiroNome = "Eligiani";
+		String segundoNome = "Monteiro";
+		
+		String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+		System.out.print(nomeCompleto);
+	}
+
+	public static String nomeCompleto(String primeiroNome, String segundoNome) {
+		return "Resutado do método " + primeiroNome.concat(" ").concat(segundoNome);
+	}
+}
